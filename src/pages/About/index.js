9f3c7f57b73f1804e84aed978571ec30911f31e3
talita-aya <tatakuriki@gmail.com>
2021-11-09 10:33:00 -0300

    about me page done with animations and routes fixed again

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { useNavigate } from 'react-router';
 
 import 'animate.css';
+import './style.css';
+import Sobre from './../../assets/sobre.svg';
+import Mim from './../../assets/mim.svg';
 
 
 function Index (){
@@ -13,10 +16,33 @@ function Index (){
         <ul>
           <li onClick={() => navigate('/')}>Home</li>
           <li className="enabled">Sobre</li>
-          <li onClick={() => navigate('/Portfolio')}>Portfólio</li>
-          <li onClick={() => navigate('/Contato')}>Contato</li>
+          <li onClick={() => navigate('/portfolio')}>Portfólio</li>
+          <li onClick={() => navigate('/contato')}>Contato</li>
         </ul>
       </header>
+
+      <section className="about-title-sobre">
+        <img className="animate__animated animate__fadeIn" src={Sobre} alt="nome background" />
+      </section>
+      
+      <section className="about-title-mim">
+        <img className="animate__animated animate__fadeIn" src={Mim} alt="nome background" />
+      </section>
+     
+      <section className="about-me-text">
+        <p>
+          Olá,
+          <br/>
+          Meu nome é Talita, sou estudante de Engenharia de Computação na Universidade Tecnológica 
+          Federal do Paraná (UTFPR) e uma grande apreciadora de arte.
+        </p>
+
+        <div className="call-to-action">
+          <input onClick={() => navigate('/contato')} type="button" value="ENTRAR EM CONTATO" />
+        </div>
+      </section>
+
+      
     </div>
   );
 }
