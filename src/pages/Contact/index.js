2669a2b825ccdc247c9c1eee11a805356e463171
talita-aya <tatakuriki@gmail.com>
2021-11-09 11:20:34 -0300

    contact page base done

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,14 +1,48 @@
-import React from 'react';
+import React from "react";
+import { useNavigate } from "react-router";
 
-import 'animate.css';
+import "animate.css";
+import "./style.css";
+import Contato1 from './../../assets/backgroundWords/contato1.svg';
+import Contato2 from './../../assets/backgroundWords/contato2.svg';
+import Instagram from './../../assets/instagram.svg';
+import Email from './../../assets/email.svg';
+import Github from './../../assets/github.svg';
+import Linkedin from './../../assets/linkedin.svg';
 
-
-const index = () => {
+function Index() {
+  const navigate = useNavigate();
   return (
     <div className="container">
-      contato
+      <header className="navbar">
+        <ul>
+          <li onClick={() => navigate("/")}>Home</li>
+          <li onClick={() => navigate("/sobre")}>Sobre</li>
+          <li onClick={() => navigate("/portfolio")}>Portfólio</li>
+          <li className="enabled">Contato</li>
+        </ul>
+      </header>
+
+      <section className="title">
+        <h1 className="contact-title animate__animated animate__fadeIn">ONDE ME ENCONTRAR</h1>
+      </section>
+
+      <section className="contact-title1">
+        <img className="animate__animated animate__fadeIn" src={Contato1} alt="palavra background" />
+      </section>
+      
+      <section className="contact-title2">
+        <img className="animate__animated animate__fadeIn" src={Contato2} alt="palavra background" />
+      </section>
+
+      <section className="socials">
+        <img src={Instagram} alt="redes sociais" />
+        <img src={Linkedin} alt="redes sociais" />
+        <img src={Github} alt="redes sociais" />
+        <img src={Email} alt="redes sociais" />
+      </section>
     </div>
   );
-}
+};
 
-export default index;
+export default Index;
