4444953889239f8056b0d8cbb1a91e7dbe5cab8c
Talita Aya <tatakuriki@gmail.com>
2023-07-31 11:30:09 -0300

    instalação de pacote para carrossel e reestruturação desta seção

--- script.js
+++ script.js
@@ -1,28 +1,14 @@
-const carousel = document.querySelector(".carousel");
-
-let isDragStart = false,
-  prevPageX,
-  prevScrollLeft;
-
-const dragStart = (e) => {
-  // updatating global variables value on mouse down event
-  isDragStart = true;
-  prevPageX = e.pageX;
-  prevScrollLeft = carousel.scrollLeft;
-};
-
-const dragging = (e) => {
-  // scrolling images/carousel to left according to mouse pointer
-  if (!isDragStart) return;
-  e.preventDefault();
-  positionDiff = e.pageX - prevPageX;
-  carousel.scrollLeft = prevScrollLeft - positionDiff;
-};
-
-const dragStop = () => {
-  isDragStart = false;
-};
-
-carousel.addEventListener("mousedown", dragStart);
-carousel.addEventListener("mousemove", dragging);
-carousel.addEventListener("mouseup", dragStop);
+let slider = tns({
+  "container": ".my-slider",
+  "items": 3,
+  "controlsContainer": false,
+  "navContainer": false,
+  "navAsThumbnails": false,
+  "autoplay": true,
+  "autoplayTimeout": 3000,
+  "autoplayButtonOutput": false,
+  "controls": false,
+  "swipeAngle": false,
+  "speed": 400,
+  "mouseDrag": true,
+});
