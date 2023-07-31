bd9dedf5193b3e10af2c1f6c48425f18b0e0acf5
Talita Aya <tatakuriki@gmail.com>
2023-07-31 13:44:44 -0300

    imagens comprimidas, carrossel funcional sem categorias

--- script.js
+++ script.js
@@ -12,3 +12,39 @@ let slider = tns({
   "speed": 400,
   "mouseDrag": true,
 });
+
+var designButton = document.getElementById("design-button")
+var desenvolvimentoButton = document.getElementById("desenvolvimento-button")
+
+var designImg = document.querySelectorAll(".design-project")
+var desenvolvimentoImg = document.querySelectorAll(".desenvolvimento-project")
+
+var projCard = document.querySelectorAll(".proj-card")
+
+designButton.addEventListener('click', showDesign)
+desenvolvimentoButton.addEventListener('click', showDesenvolvimento)
+
+function showDesign() {
+  designImg.forEach(function(img) {
+    img.style.display = 'inline-block';
+  });
+
+  desenvolvimentoImg.forEach(function(img) {
+    img.style.display = 'none';
+  })
+}
+
+function showDesenvolvimento() {
+  desenvolvimentoImg.forEach(function(img) {
+    img.style.display = 'inline-block';
+  });
+
+  designImg.forEach(function(img) {
+    img.style.display = 'none';
+  })
+}
+
+
+
+
+
